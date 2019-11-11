2ac76eee5581ab07bf44542df5ba123a4548c2b8
Burak İlkay Akgün <ilkay.akgun1@boun.edu.tr>
2019-11-11 17:52:11 +0300

    .

--- ConsoleApp1/Models/animals.cs
+++ ConsoleApp1/Models/animals.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,6 +10,7 @@ namespace ConsoleApp1.Models
     public class animal
     { 
         // models
+        //trial
         public int id { get; set; }
         public string full_name { get; set; }
         public int age { get; set; }
